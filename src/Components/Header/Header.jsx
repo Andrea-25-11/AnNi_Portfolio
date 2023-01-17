b0192ea2cfa3413913bda39a02d9acef6ce6f5c9
Karen Andrea Niño Angulo <107644961+Andrea-25-11@users.noreply.github.com>
2023-01-16 22:23:12 -0500

    Estilos Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,20 +1,45 @@
 import './Header.css';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
 
+function Header(props) {
+  const [collapsed, setCollapsed] = useState(true);
 
-function Header() {
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
   return (
-  <>
-  <nav>
-    <ul>
-      <li><a href=""><img src="" alt="" /></a></li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
-    </ul>
-  </nav>
-  </>
-
+    <div className='NavBar'>
+      <Navbar className="NavBar"color="faded" light>
+        <NavbarBrand href="/">
+        <img className='logoHeader'
+          alt="logo"
+          src="https://drive.google.com/uc?export=view&id=1HPrJzWaPITW4xrgLTtMDzybyoUaG4kpV"
+        />
+      </NavbarBrand>
+          <NavbarToggler onClick={toggleNavbar} className="me-2" />
+          <Collapse isOpen={!collapsed} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="/components/">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+      </Navbar>
+    </div>
   );
-};
+}
 
 export default Header;
