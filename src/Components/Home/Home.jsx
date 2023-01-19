3ca57859a6e949097a21e5008b2d524e5ff958c9
Karen Andrea Niño Angulo <107644961+Andrea-25-11@users.noreply.github.com>
2023-01-19 00:07:02 -0500

    Estilos TABLET

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home (){
             <Header/>
             {/* INTRO */}
             <div className='mainPpal'>
-                <div>
+                <div className="textoMain">
                     <h1>Hi! I'm Andrea</h1>
                     <img className="imgAndreaCel"src="https://drive.google.com/uc?export=view&id=1C20SJRxPzMj4W0NPfTjT9cTlm5QBHRrg" alt="" />
                     <p>I’m a full stack web developer focused on the front-end, I’m also a lawyer and a lover of everything that looks girly, cute  and pretty. <br/><br/>Enjoy seeing my profile to know my skills, projects, resume and if you are interested contact me!</p>
@@ -26,10 +26,10 @@ function Home (){
             <div className="aboutMe">
                 <h1 className="titleAboutMe">About me</h1>
                 <div className="mainAbout">
-                    <p className='pAboutMe'>Exploiting my creativity by illustrating and developing digital content of everything I had in mind was what brought me to the world of programming. What I like the most is being able to create authentic things and customize them to my liking. What I like the least is spending time looking for an error that in the end can only be a semicolon! In my free time you will surely find me dancing, admiring a sunset or pampering my cat Simón. Fun fact about me I’m also a lawyer, a slightly different profile but one to which I hope to apply my programming knowledge.</p>
+                    <p className='pAboutMe'>Exploiting my creativity by illustrating and developing digital content of everything I had in mind was what brought me to the world of programming. What I like the most is being able to create authentic things and customize them to my liking. What I like the least is spending time looking for an error that in the end can only be a semicolon! In my free time you will surely find me dancing, admiring a sunset or pampering my cat Simón. Fun fact about me I’m also a lawyer, a slightly different profile but one to which I hope to apply my programming knowledge.<br/><a className="aResume"href="">See resume<img src="https://drive.google.com/uc?export=view&id=1QTb11Xj2Bf0Y6SvQTr3XHjKxGSfePXh6" alt="" className="potion"/></a></p>
                     <img src="https://drive.google.com/uc?export=view&id=1D57nZqH5qC0_PMbKvaSRKrbcAkctFwC2" alt="" className="calderoAboutMe" />
                 </div>
-                <a className="aResume"href="">See resume<img src="https://drive.google.com/uc?export=view&id=1QTb11Xj2Bf0Y6SvQTr3XHjKxGSfePXh6" alt="" className="potion"/></a>
+                
             </div>
 
 
