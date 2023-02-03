199ed939321703ad03742c0cca68cc2011c76e62
Karen Andrea Niño Angulo <daka201828@gmail.com>
2023-02-03 17:59:24 -0500

    Activación de enlaces

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -16,7 +16,11 @@ export function Projects(props) {
               <div className="flip-card-back">
                   <p className="title">{text}</p>
                   <button className="cta">
-                      <span className="hover-underline-animation"> Visit site</span>
+                      <span className="hover-underline-animation">
+                        
+                        <a href="http://" target="_blank" rel="noopener noreferrer"><i class="ri-github-fill"></i></a>
+                        
+                        <a href={link} className="linksDespliegues"target="_blank" rel="noopener noreferrer"> Visit site</a></span>
                       <svg viewBox="0 0 46 16" height="10" width="30" xmlns={link} id="arrow-horizontal" target="_blank">
                           <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
                       </svg>
